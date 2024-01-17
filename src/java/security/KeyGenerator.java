84591ddb3db2843679c303e761b330a4c7edff49
Ander Goirigolzarri <ander.goiritur@gmail.com>
2024-01-17 10:48:54 +0100

    Update KeyGenerator.java
    KeyGenerator creates both public and private key and stores them in the security package

--- src/java/security/KeyGenerator.java
+++ src/java/security/KeyGenerator.java
@@ -1,14 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package security;
 
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.ECGenParameterSpec;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *
+ * The KeyGenerator class generates ECDH public and private keys and stores them in files.
+ * The generated keys are stored in the "security" directory relative to the application's working directory.
+ * The elliptic curve used is "secp256r1".
+ * 
  * @author Ander Goirigolzarri Iturburu
  */
 public class KeyGenerator {
-    
+
+    private static final Logger LOGGER = Logger.getLogger(KeyGenerator.class.getName());
+
+    /**
+     * The main method that launches the key generation and storage program.
+     *
+     * @param args The command-line arguments (not used in this example).
+     */
+    public static void main(String[] args) {
+        try {
+            // Generate KeyPair using ECDH algorithm
+            KeyPair keyPair = generateKeyPair();
+
+            // Store public key in a file
+            storeKey(keyPair.getPublic(), "publicKey.der");
+
+            // Store private key in a file
+            storeKey(keyPair.getPrivate(), "privateKey.der");
+
+            LOGGER.log(Level.INFO, "Public and private keys generated and stored successfully.");
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error generating or storing keys", e);
+        }
+    }
+
+    /**
+     * Generates a KeyPair using the ECDH algorithm with the "secp256r1" elliptic curve.
+     *
+     * @return The generated KeyPair.
+     * @throws NoSuchAlgorithmException if the specified algorithm is not available.
+     * @throws InvalidAlgorithmParameterException if the specified algorithm parameters are invalid.
+     */
+    private static KeyPair generateKeyPair() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
+        ECGenParameterSpec ecGenParameterSpec = new ECGenParameterSpec("secp256r1");
+        keyPairGenerator.initialize(ecGenParameterSpec);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    /**
+     * Stores the provided Key in a file with the given file name in the "security" package.
+     *
+     * @param key The Key to be stored.
+     * @param fileName The name of the file to store the key.
+     * @throws Exception if an error occurs during the file operation.
+     */
+    private static void storeKey(Key key, String fileName) throws Exception {
+        Path path = Paths.get(System.getProperty("user.dir"), "src\\java", "security", fileName);
+        Files.createDirectories(path.getParent());
+
+        try (ObjectOutputStream out = new ObjectOutputStream(Files.newOutputStream(path))) {
+            out.writeObject(key);
+        }
+    }
 }
